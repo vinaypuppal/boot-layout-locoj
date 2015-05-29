3a4a5347f19a53b98d7ee20599755c4228560f5c
vinaypuppal <puppalvinay@gmail.com>
2015-05-29 14:05:46 +0530

    profile page completed

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 jQuery(document).ready(function($) {
 	var elMap = document.getElementById('location-map');
 	var elLocInfo = document.getElementById('location-info');
+	var elLocName = document.getElementById('location-name');
 	var msg = "Sorry we are unable to get your location";
 	var lat,lng;
+	var geocoder;
 	if(Modernizr.geolocation){
 		navigator.geolocation.getCurrentPosition(success,fail);
-		elMap.textContent="Checking Location...";
+		if(elMap != null){
+			elMap.textContent="Checking Location...";
+		}	
 	}else{
 		alert("geolocation is not suppoerted by your browser please use Mordern Browser.");
-		elMap.textContent=msg;
+		if(elMap != null){
+			elMap.textContent=msg;
+		}
 	}
 
 	function success(position){
@@ -19,30 +25,65 @@ jQuery(document).ready(function($) {
 	}
 
 	function fail(msg){
-		elMap.textContent=msg;
+		if(elMap != null){
+			elMap.textContent=msg;
+		}
 		console.log(msg.code);
 	}
 
 	function initialize(){
-		elMap.textContent="Loading Map";
-		var currentLatlng = new google.maps.LatLng(lat,lng);
-		var mapOptions = {
-			center:{
-				lat:lat,
-				lng:lng
-			},
-			zoom: 15,
-			disableDefaultUI:true,
-			draggable:false,
-			scrollwheel:false
-		};
-		var locationMap = new google.maps.Map(elMap,mapOptions);
-		var marker = new google.maps.Marker({
-		      position: currentLatlng,
-		      map: locationMap,
-		      title: 'Current Location',
-		      animation:google.maps.Animation.BOUNCE
-		  });
+		var latlng = new google.maps.LatLng(lat,lng);
+		geocoder = new google.maps.Geocoder();
+		if(elMap != null){
+			var mapOptions = {
+				center:{
+					lat:lat,
+					lng:lng
+				},
+				zoom: 15,
+				disableDefaultUI:true,
+				draggable:false,
+				scrollwheel:false
+			};
+			var locationMap = new google.maps.Map(elMap,mapOptions);
+			var marker = new google.maps.Marker({
+			      position: latlng,
+			      map: locationMap,
+			      title: 'Current Location',
+			      animation:google.maps.Animation.BOUNCE
+			  });
+			var infowindow = new google.maps.InfoWindow();
+			console.log(latlng);
+			geocoder.geocode({'latLng': latlng}, function(results, status){
+				if(status == google.maps.GeocoderStatus.OK){
+					if(results[1]){
+						infowindow.setContent(results[0].formatted_address);
+	        			infowindow.open(locationMap, marker);
+	        			var area = (results[3].formatted_address).split(',');
+	        			console.log(area[0]);
+	        			elLocName.textContent = area[0];
+	        		}else{
+	        			alert('No results found');
+	        		}
+	        	}else{
+	        		alert('Geocoder failed due to: ' + status);	
+	        	}	
+			});
+		}else{
+			geocoder.geocode({'latLng': latlng}, function(results, status){
+				if(status == google.maps.GeocoderStatus.OK){
+					if(results[1]){
+						var area = (results[3].formatted_address).split(',');
+						console.log(area[0]);
+						elLocName.textContent = area[0];
+					}else{
+						alert('No results found');
+					}
+				}else{
+					alert('Geocoder failed due to: ' + status);	
+				}	
+			});
+		}
 	}
 
 });
